655eb2358c2df8701aa99c4b163f4d643732e46f
Arjun Rasodha <arjunrasodha10@gmail.com>
2022-03-13 17:25:16 -0400

    part2d lesson complete

--- lessons/part2/src/components/Note.js
+++ lessons/part2/src/components/Note.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const Note = ({ note }) => {
+const Note = ({ note, toggleImportance }) => {
+  let label = note.important ? 'make not important' : 'make important';
   return (
-    <li>{note.content}</li>
+    <li>
+      {note.content}
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
   )
 }
 
