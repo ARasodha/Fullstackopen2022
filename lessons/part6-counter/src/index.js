1418046099494cfa2cc7d623d6ada6f5384eb2fd
Arjun Rasodha <arjunrasodha10@gmail.com>
2022-03-28 12:12:40 -0400

    part6b complete

--- lessons/part6-counter/src/index.js
+++ lessons/part6-counter/src/index.js
@@ -1,29 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+// import { createStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
+
 import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
 
-const store = createStore(noteReducer)
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
   }
 })
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
+console.log(store.getState())
+
+// const reducer = combineReducers({
+//   notes: noteReducer,
+//   filter: filterReducer
+// })
+
+// const store = createStore(reducer)
 
 ReactDOM.render(
   <Provider store={store}>
